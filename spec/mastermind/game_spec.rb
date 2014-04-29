f5a3614e09ecf3fe2d1c8d4961a1ec76a815e3aa
Colin Frankish <colin.frankish@gmail.com>
2014-04-29 16:44:05 +0100

    Rspec tests refactored

--- spec/mastermind/game_spec.rb
+++ spec/mastermind/game_spec.rb
@@ -3,17 +3,17 @@ require File.join(File.dirname(__FILE__), "/../spec_helper")
 module Mastermind
   describe Game do
     context "starting up" do
+      before(:each) do
+        @messenger = double("messenger").as_null_object
+        @game = Game.new(@messenger)
+      end
       it "should send a welcome message" do
-        messenger = double("messenger").as_null_object
-        game = Game.new(messenger)
-        messenger.should_receive(:puts).with("Welcome to Mastermind!")
-        game.start
+        @messenger.should_receive(:puts).with("Welcome to Mastermind!")
+        @game.start
       end
       it "should prompt user for first guess" do
-        messenger = double("messenger").as_null_object
-        game = Game.new(messenger)
-        messenger.should_receive(:puts).with("Enter guess:")
-        game.start
+        @messenger.should_receive(:puts).with("Enter guess:")
+        @game.start
       end
     end    
   end
